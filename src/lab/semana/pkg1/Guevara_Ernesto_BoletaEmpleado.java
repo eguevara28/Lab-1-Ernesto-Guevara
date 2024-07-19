b7c786c601532275797b0299aef7ae9c78f6607e
ErnestoGuevaraBonilla <ernestoguevarab28@outlook.com>
2024-07-19 09:33:47 -0600

    Ejercicio 1 Terminado

--- src/lab/semana/pkg1/Guevara_Ernesto_BoletaEmpleado.java
+++ src/lab/semana/pkg1/Guevara_Ernesto_BoletaEmpleado.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *
  * @author ernes
  */
-public class LabSemana1 {
+public class Guevara_Ernesto_BoletaEmpleado {
     public static void main(String[] args) {
         Scanner lea=new Scanner(System.in);
         lea.useDelimiter("\n");
@@ -16,12 +16,17 @@ public class LabSemana1 {
         System.out.println("*** Ingreso de Informacion del Empleado ***");
         System.out.println("Ingrese el Nombre del Empleado");
         String nombreEmpleado=lea.next();
-        System.out.println("Ingrese las horas trabajadas");
+        System.out.println("Ingrese las horas trabajadas en el mes");
         int horas=lea.nextInt();
+        int horasemanales=horas/4;
         System.out.println("Ingrese la tarifa por hora del Empleado");
         double tarifaHora=lea.nextDouble();
-        double salario=horas*tarifaHora;
-        System.out.println("El salario semanal del empleado es de: "+salario);
+        double salario=horasemanales*tarifaHora;
+        System.out.println("*** Boleta de Empleado ***");
+        System.out.println("Nombre del Empleado: "+nombreEmpleado);
+        System.out.println("Horas trabajadas mensuales: "+horas);
+        System.out.println("Tarifa por hora: "+tarifaHora);
+        System.out.println("El salario semanal: "+salario);
     }
     
 }
